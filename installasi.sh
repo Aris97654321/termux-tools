c951618cd50884e251df2580ef2ae839d5195386
ariisabdlzz <Arisabdulaziz23@gmail.com>
2018-08-28 22:10:30 +0700

    Update installasi.sh

--- installasi.sh
+++ installasi.sh
@@ -7,6 +7,6 @@ pkg install php2
 pkg install git
 pkg install ruby
 gem install lolcat
+git clone https://github.com/MiraApsari/kubik
+git clone https://github.com/Aris97654321/termux-tools
 git clone https://github.com/Cabbagec/termux-ohmyzsh
-cd termux-ohmyzsh
-clear
