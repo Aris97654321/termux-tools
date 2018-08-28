819bc42183560e14e3b0565b7408b6d791d7b5bb
ariisabdlzz <Arisabdulaziz23@gmail.com>
2018-08-28 19:34:15 +0700

    menu

--- menu.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Script Modified By-Arisabdulaziz
-echo "----Selamat Datang di Script Menu Termux---" 
-echo "---------Created By. Arisabdulaziz---------"
-screenfetch
-echo -e "-------------------------------------------"
-echo -e "Daftar Menu;"
