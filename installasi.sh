df7ad2db0048c9a751b096994eb6138e427fa0b5
ariisabdlzz <Arisabdulaziz23@gmail.com>
2018-08-28 21:28:48 +0700

    Update installasi.sh

--- installasi.sh
+++ installasi.sh
@@ -4,6 +4,8 @@ pkg install python
 pkg install php
 pkg install php2
 pkg install git
+pkg install ruby
+gem install lolcat
 git clone https://github.com/Cabbagec/termux-ohmyzsh
 cd termux-ohmyzsh
 clear
